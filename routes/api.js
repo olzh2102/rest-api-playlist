a9de8fd0c12bf0202719125466e350b9b815a58b
olzhas2102 <olzhas2102@yahoo.com>
2018-05-07 13:23:18 +0200

    Handling POST requests

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,12 @@ router.get('/samurais', (req, res) => {
 
 // add a new samurai to the db
 router.post('/samurais', (req, res) => {
-  res.send({ type: 'POST' });
+  console.log(req.body);
+  res.send({ 
+    type: 'POST',
+    name: req.body.name,
+    rank: req.body.rank
+  });
 });
 
 
