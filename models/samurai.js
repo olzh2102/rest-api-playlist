5908d723ac25eaa8cc23a404edbce260a297b89d
olzhas2102 <olzhas2102@yahoo.com>
2018-05-07 17:21:20 +0200

    GeoJSON

--- models/samurai.js
+++ models/samurai.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// create geolocation Schema 
+const GeoSchema = new Schema({
+  type: {
+    type: String,
+    default: "Point"
+  },
+  coordinates: {
+    type: [Number],
+    index: "2dsphere"
+  }
+});
+
 // create samurai Schema and Model
 const SamuraiSchema = new Schema({
   name: {
@@ -13,9 +25,10 @@ const SamuraiSchema = new Schema({
   available: {
     type: Boolean,
     default: false
-  }
+  },
 
   // add in geo location 
+  geometry: GeoSchema
 
 });
 
