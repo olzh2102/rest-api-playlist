50801bfd336a270b4739a8f0ef43fed740726c44
olzhas2102 <olzhas2102@yahoo.com>
2018-05-07 14:12:21 +0200

    Error handling

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.use(bodyParser.json());
 // initialize routes
 app.use('/api', require('./routes/api'));
 
+// error handling middleware 
+app.use((err, req, res, next) => {
+  //console.log(err);
+  res.status(422).send({ error: err.message });
+});
+
 // listen for requests 
 app.listen(process.env.port || 4000, () => {
   console.log('now listening for requests');
--- routes/api.js
+++ routes/api.js
@@ -3,27 +3,27 @@ const router = express.Router();
 const Samurai = require('../models/samurai');
 
 // get a list of samurais from the db 
-router.get('/samurais', (req, res) => {
+router.get('/samurais', (req, res, next) => {
   res.send({ type: 'GET' });
 });
 
 // add a new samurai to the db
-router.post('/samurais', (req, res) => {
+router.post('/samurais', (req, res, next) => {
   // var samurai = new Samurai(req.body);
   // samurai.save();
   Samurai.create(req.body).then((samurai) => {
     res.send(samurai);
-  }); 
+  }).catch(next); 
 });
 
 
 // update a samurai in the db
-router.put('/samurais/:id', (req, res) => {
+router.put('/samurais/:id', (req, res, next) => {
   res.send({ type: 'PUT' });
 });
 
 // delete a samurai from the db
-router.delete('/samurais/:id', (req, res) => {
+router.delete('/samurais/:id', (req, res, next) => {
   res.send({ type: 'DELETE' });
 });
 
